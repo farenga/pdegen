01036157bc921f72762c59bac59591bd81d95d9b
farenga <nicola.farenga@gmail.com>
2023-03-08 22:18:41 +0100

    [add] reqs

--- setup.py
+++ setup.py
@@ -9,7 +9,18 @@ setuptools.setup(
     description="Partial Differential Equations Dataset Generation Library",
     url="https://github.com/farenga/pdegen",
     packages=setuptools.find_packages(),
-    install_requires=['torch>=1.5.0', 'numpy>=1.21.0'],
+    install_requires=[
+    'fenics>=2019.1.0',
+    'fenics_dolfin>=2019.2.0.dev0',
+    'h5py>=3.8.0',
+    'h5py._debian_h5py_serial>=3.6.0',
+    'mshr>=2019.2.0.dev0',
+    'numpy>=1.21.5',
+    'PyYAML>=6.0',
+    'setuptools>=59.6.0',
+    'torch>=1.13.1',
+    'vtk>=9.2.6'
+    ],
     python_requires='~=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
